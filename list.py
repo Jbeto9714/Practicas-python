cb99ef8f8a88826ecc3852ff19f646124dcf15d2
Alberto Suarez <beto9714@gmail.com>
2024-04-10 01:17:02 -0600

    tercer practica

---
+++ list.py
@@ -0,0 +1,10 @@
+#Las listas son estructuras de datos que nos permiten almacenar distintos valores (equivalentes a los arrays)
+#Las listas son mutables
+
+list1 = ["Oscar", 24, 45, 57]
+print(list1)
+
+#Para agregar elementos 
+
+list1.append("nuevo elemento")
+print(list1)
---
+++ tuplas.py
@@ -0,0 +1,10 @@
+tupla1=(1,2,3)
+print(tupla1)
+tupla2=(7, "oscar", True, 401.234) 
+print(tupla2)
+tupla3=(9, 3, (4, 5, 6))
+print(tupla3)
+
+#Haciendo slicing
+
+print(tupla2[0:3])
